07adbe6653c85d1e3d6888ba691c87c5231d24f3
RodrigoMathias.14 <rod.cmathiasc@gmail.com>
2025-11-04 22:39:08 -0300

    Add files via upload
    # Sistema de Biblioteca em TypeScript
    
    ## 1. Descrição
    
    Este projeto apresenta uma simulação de um sistema de biblioteca, desenvolvida em TypeScript, com funcionalidades de cadastro, empréstimo e devolução de livros.  
    O objetivo é demonstrar, de forma prática, conceitos de Programação Orientada a Objetos (POO) e o uso de métodos assíncronos (`async/await`).
    
    ---
    
    ## 2. Estrutura do Projeto:
    
    O sistema é composto por duas classes principais:
    
    - Livro: representa um livro, com título, autor e status de disponibilidade.  
    - Biblioteca: gerencia o acervo de livros, permitindo adicionar, emprestar e devolver exemplares.
    
    ---
    
    ## 3. Tecnologias Utilizadas:
    
    - TypeScript  
    - Node.js  
    - ts-node

---
+++ Exercicio_TS_M3.ts
@@ -0,0 +1,72 @@
+class Livro {
+  Titulo: string;
+  Autor: string;
+  Disponivel: boolean;
+
+  constructor (titulo: string, autor: string, disponivel: boolean = true) {
+    this.Titulo = titulo;
+    this.Autor = autor;
+    this.Disponivel = disponivel;
+  }
+}
+
+class Biblioteca {
+  Nome: string;
+  acervoLivros: Livro[] = [];
+
+  constructor (nome: string) {
+    this.Nome = nome;
+  }
+
+  addLivro (livro: Livro): void {
+    this.acervoLivros.push (livro);
+    console.log(`O livro "${livro.Titulo}" foi adicionado à biblioteca ${this.Nome}!`);
+  }
+
+  async emprestarLivro (titulo: string): Promise <Livro | null> {
+
+  const livro = this.acervoLivros.find(l => l.Titulo === titulo);
+
+    if (!livro) {
+      console.log(`O livro "${titulo}" não foi encontrado no acervo.`);
+      return null;
+    }
+
+    if (!livro.Disponivel) {
+      console.log(`O livro "${livro.Titulo}" não está disponível.`);
+      return null;
+    }
+
+    livro.Disponivel = false;
+    console.log(`O livro "${livro.Titulo}" foi emprestado com sucesso.`);
+    return livro;
+  }
+
+  devolverLivro (livro: Livro): void {
+    const encontrado = this.acervoLivros.find (l => l === livro);
+
+    if (encontrado) {
+      encontrado.Disponivel = true;
+      console.log (`O livro "${livro.Titulo}" foi devolvido com sucesso.`);
+    } else {
+      console.log (`O livro "${livro.Titulo}" não pertence ao acervo da biblioteca ${this.Nome}.`);
+    }
+  }
+}
+
+async function main() {
+  const biblioteca = new Biblioteca ("Biblioteca RCMC");
+
+  const livro1 = new Livro ("O Hobbit", "J. R. R. Tolkien");
+  const livro2 = new Livro ("A Ilha do Tesouro", "Robert Louis Stevenson");
+
+  biblioteca.addLivro (livro1);
+  biblioteca.addLivro (livro2);
+
+  const emprestado = await biblioteca.emprestarLivro ("A Ilha do Tesouro");
+  await biblioteca.emprestarLivro ("A Ilha do Tesouro");
+  if (emprestado) biblioteca.devolverLivro (emprestado);
+  await biblioteca.emprestarLivro ("A Ilha do Tesouro"); 
+}
+
+main();
